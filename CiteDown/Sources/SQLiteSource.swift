198a3c80b03ddc886f81471002cff9dbf60985c7
Jason Abbott <jason@toba.io>
2022-10-23 17:20:17 -0600

    wip

--- CiteDown/Sources/SQLiteSource.swift
+++ CiteDown/Sources/SQLiteSource.swift
@@ -1,25 +1,40 @@
 import Foundation
 import GRDB
 
+// https://github.com/groue/GRDB.swift
 class SQLiteSource {
     private var db: DatabaseQueue
     private var config: Configuration
+    private var path: String
 
     init(path: String) {
         config = Configuration()
         config.readonly = true
-        config.label = "Zotero" // Useful when your app opens multiple databases
+        config.label = "Zotero"
         config.maximumReaderCount = 10
+
+        self.path = path
 
         #if DEBUG
             // Protect sensitive information by enabling verbose debugging in
             // DEBUG builds only
             config.publicStatementArguments = true
         #endif
+    }
 
-        db = try DatabaseQueue( // or DatabasePool
-            path: path,
-            configuration: config
-        )
+    func read(sql _: String) {
+        db = try DatabaseQueue(path: path, configuration: config)
+
+        try db.read { db in
+            if let row = try Row.fetchOne(
+                db,
+                sql: "SELECT * FROM wine WHERE id = ?",
+                arguments: [1]
+            ) {
+                let name: String = row["name"]
+                let color: Color = row["color"]
+                print(name, color)
+            }
+        }
     }
 }
